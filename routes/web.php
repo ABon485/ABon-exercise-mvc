0ec26fb294b378a875df608b126103123d96471e
Bon <bon.a25@student.passerellesnumeriques.org>
2024-03-15 23:52:22 +0700

    fisrt commit

---
+++ routes/web.php
@@ -0,0 +1,32 @@
+<?php
+use App\Models\Fruit;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\FruitController;
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/myview/{user}', function ($user) {
+    return view('home', ['username' => $user]);
+});
+Route::get('/fruits', function() {
+    return Fruit::all();
+});
+Route::get('/fruits', function () {
+
+    return Fruit::all();
+
+});
+
+Route::get('/showFruits', [FruitController::class, 'getFruits']);
